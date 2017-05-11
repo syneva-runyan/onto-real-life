1fc2eeb7e50bfe75f6facd411617250f5ad9889e
syneva-runyan <syneva@gmail.com>
2017-05-11 15:04:26 -0400

    fixed windows dev issues

--- source/js/views/App/index.jsx
+++ source/js/views/App/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Header } from 'components/Global';
 
 const propTypes = {
-  location: React.propTypes.object,
+  location: React.PropTypes.object,
 };
 
 const defaultProps = {
