88ec35564ec3c5f59b5e8500eaaa26ad3854fcf4
syneva-runyan <syneva@gmail.com>
2017-05-12 18:09:06 -0400

    removed header border snapping

--- source/js/components/Global/Header.jsx
+++ source/js/components/Global/Header.jsx
@@ -16,38 +16,6 @@ const defaultProps = {
 };
 
 export default class Header extends Component {
-  componentDidMount() {
-    this.adjBorder();
-
-    this.resizeListener = window.addEventListener('resize', () => {
-      this.adjBorder();
-    });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(this.resizeListener);
-  }
-
-  adjBorder() {
-    const border = document.getElementById('headerBorder');
-    border.style.width = '100%';
-    const elWidth = border.getBoundingClientRect().width;
-    console.log('el width', elWidth);
-    this.snapWidth(border, chevronWidth, elWidth);
-  }
-
-  // Snap width of provided el
-  // to nearest round number.
-  snapWidth(element, snapWidth, elWidth) {
-    const widthMod = elWidth % snapWidth;
-    console.log(widthMod);
-    if (widthMod !== 0) {
-      // eslint-disable-next-line
-      element.style.width = elWidth - widthMod + 1 + 'px';
-      console.log(elWidth - widthMod + 'px');
-    }
-  }
-
   render() {
     return (
       <div className='header'>
--- tests/components/Header.spec.jsx
+++ tests/components/Header.spec.jsx
@@ -8,14 +8,7 @@ describe('Header', () => {
 	beforeEach(() => {
 		component = shallow(<Header />);
 	})
-	describe('snap width', () => {
-		it('should round down width of provided element to fit to provided snapWidth', () => {
-			let fakeElement = document.createElement('p');
-			fakeElement.style.width = '503px';
-
-			component.instance().snapWidth(fakeElement, 5, 503);
-
-			expect(fakeElement.style.width).toEqual('501px');
-		});
+	it('should exist', () => {
+		expect(component).toBeTruthy();
 	});
 });
