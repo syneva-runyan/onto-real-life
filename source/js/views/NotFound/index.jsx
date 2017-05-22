00b864aa04373769e97af8bf9c09cc00b2d8c2cf
syneva-runyan <syneva@gmail.com>
2017-05-22 05:14:12 -0400

    finished adding blog post content and fixed other page place holders. Slight post style fixes

--- source/js/views/NotFound/index.jsx
+++ source/js/views/NotFound/index.jsx
@@ -4,7 +4,8 @@ export default class NotFound extends Component {
   render() {
     return (
       <div className='NotFound'>
-        <h2>Not Found</h2>
+        <h3>Uh oh!</h3>
+        <p>This site is currently under construction, and the page you're looking for is not found.  Come back again soon!</p>
       </div>
     );
   }
