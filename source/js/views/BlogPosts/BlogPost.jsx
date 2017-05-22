546d867d8cec46362f6451284a04573e9fdc0c46
syneva-runyan <syneva@gmail.com>
2017-05-22 01:42:02 -0400

    setup basic editor and viewer

--- source/js/views/BlogPosts/BlogPost.jsx
+++ source/js/views/BlogPosts/BlogPost.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { PostTitle } from '../../components/BlogPosts';
+import { PostTitle, PostContent } from '../../components/BlogPosts';
 import postCatalog from '../../../data/posts';
 
 const propTypes = {};
@@ -18,7 +18,7 @@ export default class BlogPost extends Component {
           datePublished={ post.datePublished }
           tagLine={ post.tagLine }
          />
-        <p>Blog Post Content Goes Here</p>
+        <PostContent postId={this.props.params.postId} />
       </div>
     );
   }
