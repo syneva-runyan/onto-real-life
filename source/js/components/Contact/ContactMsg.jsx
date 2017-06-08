4860d7ffcd0890502b64e8a1e1764114986b178e
syneva-runyan <syneva@gmail.com>
2017-06-07 20:43:15 -0400

    fixed state setting bug in contect form

--- source/js/components/Contact/ContactMsg.jsx
+++ source/js/components/Contact/ContactMsg.jsx
@@ -13,15 +13,48 @@ export default class ContactMsg extends Component {
             senderEmail: "",
             msg: "",
         }
+
+        this.boundSubjectChange = this.onChange.bind(this, 'subject');
+        this.boundEmailChange = this.onChange.bind(this, 'senderEmail');
+        this.boundMsgChange = this.onChange.bind(this, 'msg');
+    }
+
+    onChange(field, e) {
+        const newValue = e.target.value;
+        const keyValuePair = {};
+        keyValuePair[field] = newValue;
+
+        this.setState(keyValuePair);
     }
 
     render() {
         return (
           <form action="./scripts/sendEmail.php">
-            <input className="contactMsg__subject" name="contact-subject" id="contact-subject" placeholder="Subject" value={this.state.subject} />
-            <input className="contactMsg__yourEmail" name="message" id="contact-email" placeholder="Your email" value={this.state.senderEmail} />
+            <input
+              className="contactMsg__subject"
+              name="contact-subject"
+              id="contact-subject"
+              placeholder="Subject"
+              value={this.state.subject}
+              onChange={this.boundSubjectChange}
+            />
+            <input
+              className="contactMsg__yourEmail"
+              name="message"
+              id="contact-email"
+              placeholder="Your email"
+              value={this.state.senderEmail} 
+              onChange={this.boundEmailChange}
+            />
             <br />
-            <textarea placeholder="Hi! I love your site..." name="message" id="contact-message">{this.state.msg}</textarea>
+            <textarea
+              placeholder="Hi! I love your site..."
+              name="message"
+              id="contact-message"
+              onChange={this.boundMsgChange}
+            >
+              {this.state.msg}
+            </textarea>
             <p>*All fields required</p>
             <submit>Submit</submit>
           </form>
