64cb6c224c1f0c3101bfd04d7bc846721d2cfc2b
syneva-runyan <syneva@gmail.com>
2017-06-29 04:44:32 -0400

    executed prettier with options

--- source/js/actions/app.js
+++ source/js/actions/app.js
@@ -9,41 +9,42 @@ export const TEST_ASYNC_ACTION_SUCCESS = 'TEST_ASYNC_ACTION_SUCCESS';
 // Test action
 
 export function testAction() {
-  return {
-    type: TEST_ACTION,
-  };
+	return {
+		type: TEST_ACTION,
+	};
 }
 
 // Async action example
 
 function testAsyncStart() {
-  return {
-    type: TEST_ASYNC_ACTION_START,
-  };
+	return {
+		type: TEST_ASYNC_ACTION_START,
+	};
 }
 
 function testAsyncSuccess(data) {
-  return {
-    type: TEST_ASYNC_ACTION_SUCCESS,
-    data,
-  };
+	return {
+		type: TEST_ASYNC_ACTION_SUCCESS,
+		data,
+	};
 }
 
 function testAsyncError(error) {
-  return {
-    type: TEST_ASYNC_ACTION_ERROR,
-    error,
-  };
+	return {
+		type: TEST_ASYNC_ACTION_ERROR,
+		error,
+	};
 }
 
 export function testAsync() {
-  return function (dispatch) {
-    dispatch(testAsyncStart());
+	return function(dispatch) {
+		dispatch(testAsyncStart());
 
-    api.testAsync()
-      .then(data => dispatch(testAsyncSuccess(data)))
-      .catch(error => dispatch(testAsyncError(error)));
-  };
+		api
+			.testAsync()
+			.then(data => dispatch(testAsyncSuccess(data)))
+			.catch(error => dispatch(testAsyncError(error)));
+	};
 }
 
 // Update
