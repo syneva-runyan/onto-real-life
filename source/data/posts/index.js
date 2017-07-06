8a46b57e861de18c4901867d704d3577b282d4ed
syneva-runyan <syneva@gmail.com>
2017-07-05 21:33:19 -0400

    finished blog post content

--- source/data/posts/index.js
+++ source/data/posts/index.js
@@ -1,13 +1,15 @@
-var barcelona = require('./barcelona/config.json');
-var eatingAlone = require('./eatingAlone/config.json');
-var malta = require('./malta/config.json');
-var morocco = require('./morocco/config.json');
-var seeTheWorld = require('./seeTheWorld/config.json');
-var newZealandNorthOverview = require('./newZealandNorthOverview/config.json');
-var newZealandNorthHiking =require('./newZealandNorthHiking/config.json');
-var newZealandSouth = require('./newZealandSouth/config.json');
+var barcelona = require("./barcelona/config.json");
+var eatingAlone = require("./eatingAlone/config.json");
+var malta = require("./malta/config.json");
+var morocco = require("./morocco/config.json");
+var seeTheWorld = require("./seeTheWorld/config.json");
+var newZealandNorthOverview = require("./newZealandNorthOverview/config.json");
+var newZealandNorthHiking = require("./newZealandNorthHiking/config.json");
+var newZealandSouth = require("./newZealandSouth/config.json");
+var nzTravelThoughts = require("./nzTravelThoughts/config.json");
 
-module.exports = { 
+module.exports = {
+  nzTravelThoughts,
   newZealandSouth,
   newZealandNorthOverview,
   newZealandNorthHiking,
