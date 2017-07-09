dc7f8b22094f93ed34bd3e9fa9f8522a09339e51
syneva-runyan <syneva@gmail.com>
2017-07-09 19:54:21 -0400

    added blog posts view tests

--- tests/views/BlogPosts/BlogPosts.spec.jsx
+++ tests/views/BlogPosts/BlogPosts.spec.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { BlogPosts } from "../../../source/js/views/BlogPosts/index.js";
+import {
+  PostTitle,
+  PostContent
+} from "../../../source/js/components/BlogPosts";
+import PostCollection from "../../../source/js/views/BlogPosts/PostCollection";
+import postCatalog from "../../../source/data/posts";
 import { shallow } from "enzyme";
+
+jest.mock("../../../source/data/posts");
 
 describe("BlogPosts", () => {
   let component;
@@ -10,5 +18,21 @@ describe("BlogPosts", () => {
   });
   it("should exist", () => {
     expect(component).toBeTruthy();
+  });
+  it("should render blog post collection if not provided a postId", () => {
+    const postCollection = component.find(PostCollection);
+    expect(postCollection.length).toEqual(1);
+  });
+  it("should render blog post title and content iff proved postId", () => {
+    const postParams = {
+      postId: "mockedPost"
+    };
+    component = shallow(<BlogPosts params={postParams} />);
+
+    const blogPostTitle = component.find(PostTitle);
+    const blogPostContent = component.find(PostContent);
+
+    expect(blogPostContent.length).toEqual(1);
+    expect(blogPostTitle.length).toEqual(1);
   });
 });
