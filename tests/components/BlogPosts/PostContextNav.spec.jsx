c7631e9a095a1fba12f2cdc6c7aa65a5d4dc0615
syneva-runyan <syneva@gmail.com>
2017-07-19 03:21:20 -0400

    conditionally rendering nav ctas

--- tests/components/BlogPosts/PostContextNav.spec.jsx
+++ tests/components/BlogPosts/PostContextNav.spec.jsx
@@ -4,11 +4,33 @@ import { shallow } from "enzyme";
 
 describe("Post Context Nav", () => {
   let component;
+  const entryDetails = {
+    id: "id",
+    title: "title",
+    tagLine: "tagLine",
+  };
 
   beforeEach(() => {
     component = shallow(<PostContextNav />);
   });
   it("should exist", () => {
     expect(component).toBeTruthy();
+  });
+  it("should render a next btn iff provided next post", () => {
+    let nextBtn = component.find(".postContextNav--next");
+    expect(nextBtn.length).toEqual(0);
+
+    component = shallow(<PostcontextNav next={entryDetails} />);
+    nextBtn = component.find(".postContextNav--next");
+    expect(nextBtn.length).toEqual(1);
+  });
+  it("should render a previous btn iff provided previous post", () => {
+    let prevBtn = component.find(".postContextNav--next");
+    expect(prevBtn.length).toEqual(0);
+
+    component = shallow(<PostcontextNav next={entryDetails} />);
+    prevBtn = component.find(".postContextNav--next");
+
+    expect(prevBtn.length).toEqual(1);
   });
 });
