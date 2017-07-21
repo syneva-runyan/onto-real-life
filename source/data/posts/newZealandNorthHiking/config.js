a2d0d2c23e6c60be058f8a59feaf7aef0c248cce
syneva-runyan <syneva@gmail.com>
2017-07-21 02:49:43 -0400

    fixed newzealand north hiking

--- source/data/posts/newZealandNorthHiking/config.js
+++ source/data/posts/newZealandNorthHiking/config.js
@@ -1,6 +1,9 @@
-{
+import Content from "./content.jsx";
+
+module.exports = {
   "id": "newZealandNorthHiking",
   "title": "NZ North Island Hiking",
   "datePublished": "9 June, 2017",
-  "tagLine": "Hiking Catalogue"
+  "tagLine": "Hiking Catalogue",
+  Content,
 }
