fd7644c672cccaec8118cbe7669bfff89728e338
syneva-runyan <syneva@gmail.com>
2017-07-22 03:11:42 -0400

    reverted blog posts back to html

--- source/js/data/posts/eatingAlone/config.js
+++
@@ -1,8 +0,0 @@
-import Content from "./content.jsx";
-
-module.exports = {
-  id: "eatingAlone",
-  title: "Eating Alone",
-  datePublished: "5 August, 2015",
-  Content,
-};
--- source/js/data/posts/seeTheWorld/config.js
+++
@@ -1,8 +0,0 @@
-import Content from "./content.jsx";
-
-module.exports = {
-  id: "seeTheWorld",
-  title: "Goal: See the World",
-  datePublished: "5 March, 2017",
-  Content,
-};
