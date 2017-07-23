787328e2f53c591eb2a2f393c1a66af03c0389ba
syneva-runyan <syneva@gmail.com>
2017-07-23 05:15:07 -0400

    getting different routes to build statically

--- source/js/index.js
+++ source/js/index.js
@@ -1,14 +1,44 @@
-import React from "react";
-import { Router } from "react-router";
-import { renderToStaticMarkup } from "react-dom";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import { ReactDOM } from 'react-dom'
+import ReactDOMServer from 'react-dom/server';
+import routes from './routes';
 
-import routes from "routes";
+const Html = props =>
+(<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Onto Real life" />
+    <meta property="og:description" content="Going places and taking pictures." />
+    <meta property="og:image" content="http://ontoreallife.com/assets/img/blogs/newZealandSouth/lakeMatheson.jpg" />
+    <title>Onto real life</title>
+  </head>
+  <body>
+    <div id="app">
+        {props.children}
+    </div>
+    <script src="/app.js"></script>
+  </body>
+</html>);
 
+if (typeof document != 'undefined') {
+  ReactDOM.render(
+    <BrowserRouter>{routes}</BrowserRouter>,
+    document.getElementById('#app')
+  )
+}
 
-// Load SCSS
-import "../scss/app.scss";
-
-// Render it to DOM
-module.exports = function render(locals, callback) {
-    Router.run(routes, )
+/**
+ * This function is the global export that static-render-webpack-plugin uses
+ */
+/* Exported static site renderer */
+export default (locals, callback) => {
+    var html = ReactDOMServer.renderToStaticMarkup(
+      (<Html>
+        {routes(locals)}
+      </Html>
+    ), locals);
+    callback(null, html)
 }
