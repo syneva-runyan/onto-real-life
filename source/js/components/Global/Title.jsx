118422f7031557686efaa8edbe743e38cdd7911e
syneva-runyan <syneva@gmail.com>
2017-07-25 09:05:06 -0400

    working on dev build fixes

--- source/js/components/Global/Title.jsx
+++ source/js/components/Global/Title.jsx
@@ -5,7 +5,7 @@ import { routeCodes } from "../../routes";
 export default class Title extends Component {
   render() {
     return (
-      <Link className="title" to={routeCodes.HOME}>
+      <a className="title" href={routeCodes.HOME}>
         <span className="title__block">
           <span>On</span>
           <br />
@@ -17,7 +17,7 @@ export default class Title extends Component {
         <span className="title__text">
           L<span className="title--desktop">IFE</span>
         </span>
-      </Link>
+      </a>
     );
   }
 }
--- webpack-helpers/webpack.common.js
+++ webpack-helpers/webpack.common.js
@@ -1,33 +1,19 @@
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const StaticSiteGeneratorPlugin = require("static-site-generator-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 const path = require("path");
-const ejs = require("ejs");
-const fs = require("fs");
+
 const nodeEnv = process.env.NODE_ENV || "development";
 const sourcePath = path.join(__dirname, "../source");
 const imgPath = path.join(__dirname, "../source/assets/img");
 const jsSourcePath = path.join(__dirname, "./source/js");
 const buildPath = path.join(__dirname, "../build");
 
-var template = ejs.compile(fs.readFileSync(path.join(__dirname, '../source/template.ejs'), 'utf-8'))
-const routeData = require('./route-data');
-
 module.exports = {
     plugins: [
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: "vendor",
-        //     filename: "vendor-[hash].js",
-        //     minChunks(module) {
-        //     const context = module.context;
-        //     return context && context.indexOf("node_modules") >= 0;
-        //     }
-        // }),
         new webpack.DefinePlugin({
             "process.env": {
-            NODE_ENV: JSON.stringify(nodeEnv),
+                NODE_ENV: JSON.stringify(nodeEnv),
             }
         }),
         new webpack.NamedModulesPlugin(),
@@ -44,30 +30,13 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
-        }),
-        new StaticSiteGeneratorPlugin({
-            entry: 'app',
-            paths: routeData.paths,
-            locals: {
-                template: template
-            },
-            globals: {
-                window: {
-                    XMLHttpRequest: {}
-                }
-            }
-        }),
+        }), 
         new CopyWebpackPlugin([
             {
             from: "../assets/scripts/sendEmail.php",
             to: "scripts/sendEmail.php"
             },
-        ]),
-        // new HtmlWebpackPlugin({
-        //     template: path.join(sourcePath, "index.html"),
-        //     path: buildPath,
-        //     filename: "index.html",
-        // }),
+        ])
     ],
     rules: [{
         test: /\.css$/,
