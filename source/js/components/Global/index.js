27d1b7906bffa2269dc827d8ad3e00e4d4507c28
syneva-runyan <syneva@gmail.com>
2017-07-29 10:23:16 -0400

    finished first third of post and created responsive img component

--- source/js/components/Global/index.js
+++ source/js/components/Global/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Menu from "./Menu";
 import NotFound from "./NotFound";
+import ResponsiveImg from "./ResponsiveImg";
 
 module.exports = {
   Header,
   Menu,
   NotFound,
+  ResponsiveImg,
 };
