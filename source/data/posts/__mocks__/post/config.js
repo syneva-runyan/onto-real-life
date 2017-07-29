9adc18bd552e4ed38b54d19c4de9d31a07406058
syneva-runyan <syneva@gmail.com>
2017-07-29 00:39:05 -0400

    fixed tests

---
+++ source/data/posts/__mocks__/post/config.js
@@ -0,0 +1,9 @@
+import MockComponent from "./MockComponent";
+module.exports =
+{
+  "id": "somePost",
+  "title": "Post Title",
+  "datePublished": "5 July, 2017",
+  "tagLine": "tagline",
+  "component": MockComponent,
+}
--- source/js/views/BlogPosts/index.js
+++ source/js/views/BlogPosts/index.js
@@ -1,7 +1,9 @@
 import BlogPosts from "./BlogPosts";
 import PostCollection from "./PostCollection";
+import Post from "./Post";
 
 module.exports = {
   BlogPosts,
   PostCollection,
+  Post,
 };
