4559d6a77d854ef575772c31d054cbf0635aa826
syneva-runyan <syneva@gmail.com>
2017-07-29 06:22:17 -0400

    added previewer images

---
+++ source/data/posts/java/config.js
@@ -0,0 +1,9 @@
+import Content from "./Content.jsx";
+
+module.exports = {
+  id: "java",
+  title: "Java",
+  tagLine: "Land of the Sunrise Tours",
+  datePublished: "29 July, 2017",
+  component: Content,
+};
