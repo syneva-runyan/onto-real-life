bbfc556a69d4517ac72b62299907c502c71ddf7a
syneva-runyan <syneva@gmail.com>
2017-07-29 01:17:58 -0400

    adj nz north overview post content

--- source/data/posts/newZealandNorthOverview/config.js
+++ source/data/posts/newZealandNorthOverview/config.js
@@ -1,6 +1,9 @@
-{
+import Content from "./Content.jsx";
+
+module.exports = {
   "id": "newZealandNorthOverview",
   "title": "New Zealand - North Island",
   "datePublished": "9 June, 2017",
-  "tagLine": "Overview"
+  "tagLine": "Overview",
+  "component": Content,
 }
