be146cc0a47a558351eae724dd28904a1abcfd96
syneva-runyan <syneva@gmail.com>
2017-08-03 04:08:26 -0400

    added java how to content

--- source/data/posts/index.js
+++ source/data/posts/index.js
@@ -8,8 +8,10 @@ const newZealandNorthHiking = require("./newZealandNorthHiking/config.js");
 const newZealandSouth = require("./newZealandSouth/config.js");
 const nzTravelThoughts = require("./nzTravelThoughts/config.js");
 const java = require("./java/config.js");
+const javaTravel= require("./javaTravel/config.js");
 
 module.exports = {
+  javaTravel,
   java,
   nzTravelThoughts,
   newZealandSouth,
--- source/data/posts/javaActivities/config.js
+++
@@ -1,9 +0,0 @@
-import Content from "./Content.jsx";
-
-module.exports = {
-  id: "java",
-  title: "Java",
-  tagLine: "What to do and How",
-  datePublished: "31 July, 2017",
-  component: Content,
-};
