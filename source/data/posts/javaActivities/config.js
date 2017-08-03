176fae0e8edda7f1d28119d0b2a6eba6d5306949
syneva-runyan <syneva@gmail.com>
2017-08-03 00:15:56 -0400

    finished main java post

---
+++ source/data/posts/javaActivities/config.js
@@ -0,0 +1,9 @@
+import Content from "./Content.jsx";
+
+module.exports = {
+  id: "java",
+  title: "Java",
+  tagLine: "What to do and How",
+  datePublished: "31 July, 2017",
+  component: Content,
+};
