4b8d35b0108fede741f5855a50fc6b5e087f13b1
syneva-runyan <syneva@gmail.com>
2017-08-03 00:15:56 -0400

    finished main java post

--- source/js/utils/imgRename.js
+++ source/js/utils/imgRename.js
@@ -1,13 +1,23 @@
-var fs = require('fs');
+// Looks for files with original prefix and suffix combinations in input directory
+// and renames them to provided img name with corresponding suffix combinations.
+
+const fs = require('fs');
+const assert = require('assert');
+const argv = require('minimist')(process.argv.slice(2));
+
 
 const ogPrefix = "img";
 const fileExt = ".jpg";
 const inputDir = "C:/Users/Syneva/Development/onto-real-life/source/assets/img/blogs/java";
-const newPrefix = "scooter2";
-
 const suffixes = ["", "--sm", "--md", "--lg"];
 
+// Check to make sure imgName is provided in argumes
+assert.ok(!!argv["imgName"], "Please provide arg 'imgName' with value");
+const newPrefix = argv["imgName"];
+
+// Iterate through ogPrefix/suffix combinations and rename.
 for (let suffix of suffixes) {
+    console.log(`Renaming ${ogPrefix}${suffix}${fileExt} to ${newPrefix}${suffix}${fileExt}`);
 
     fs.rename(`${inputDir}\\${ogPrefix}${suffix}${fileExt}`, `${inputDir}\\${newPrefix}${suffix}${fileExt}`, function(err) {
         if (err) {
