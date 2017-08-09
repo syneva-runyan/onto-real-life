df2a34bd610b86980ced4a9826937d260374ea15
syneva-runyan <syneva@gmail.com>
2017-08-09 01:41:25 -0400

    executed prettier

--- source/js/components/Global/Video.jsx
+++ source/js/components/Global/Video.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const propTypes = {
-    src: PropTypes.string,
-    controls: PropTypes.bool
+  src: PropTypes.string,
+  controls: PropTypes.bool,
 };
 
 const defafultProps = {
-    src: "",
-    controls: true
-}
+  src: "",
+  controls: true,
+};
 
 export default function Video(props) {
-    return (
-      <video className="video" src={props.src} controls={props.controls} />
-    );
+  return <video className="video" src={props.src} controls={props.controls} />;
 }
+
+Video.propTypes = propTypes;
+Video.defafultProps = defafultProps;
