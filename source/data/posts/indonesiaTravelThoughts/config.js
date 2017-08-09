b926b44e1baebfdb4afd336ea4476a643ff132f1
syneva-runyan <syneva@gmail.com>
2017-08-09 00:35:45 -0400

    wrote content for indonesia thoughts

---
+++ source/data/posts/indonesiaTravelThoughts/config.js
@@ -0,0 +1,9 @@
+import Content from "./Content.jsx";
+
+module.exports = {
+  id: "indonesiaTravelThoughts",
+  title: "Indonesia Travel Thoughts",
+  tagLine: "Q&A",
+  datePublished: "8 August, 2017",
+  component: Content,
+};
