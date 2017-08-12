dbf3dc36f46c499463163a085c196698774f49e9
syneva-runyan <syneva@gmail.com>
2017-08-12 05:03:09 -0400

    Created carousel base display and style

---
+++ tests/components/Carousel/Carousel.spec.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Carousel } from "../../../source/js/components/Carousel";
+import { shallow, render } from "enzyme";
+
+jest.mock("../../../source/js/utils/post-helper");
+
+describe("Post Content", () => {
+    let component;
+  const exSlide = "Ex Slide content";
+  const mockSlides = [exSlide, exSlide, exSlide] ;
+  const exAniDuration = "2s"
+
+  beforeEach(() => {
+    component = shallow(<Carousel aniDuration={exAniDuration} slides={mockSlides} />);
+  });
+  it("should exist", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should generate a slide for every 'slide'", () => {
+   const slides = component.find(".carousel__slide");
+
+   expect(slides.length).toEqual(3);
+  });
+  
+  it("should set css' animation duration from provided prop", () => {
+      expect(component.props().style.animationDuration).toEqual(exAniDuration);
+  })
+});
