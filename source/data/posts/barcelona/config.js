fc979cd141ca6199877bba0a600b34516721c001
syneva-runyan <syneva@gmail.com>
2017-08-21 00:40:59 -0400

    added tags to blog post configs

--- source/data/posts/barcelona/config.js
+++ source/data/posts/barcelona/config.js
@@ -2,6 +2,7 @@ import Content from "./Content.jsx";
 
 module.exports = {
   id: "barcelona",
+  tags: ["barcelona", "spain", "europe", "montserrat", "chupitos", "gaudi", "sagrada familia", "yellow car", "little yellow car", "snocks", "black mother", "catalonia", "hike", "hiking"],
   title: "Barcelona",
   tagLine: "Don't Pack While Drunk",
   datePublished: "16 March, 2017",
--- source/data/posts/indonesiaTravelThoughts/config.js
+++ source/data/posts/indonesiaTravelThoughts/config.js
@@ -2,6 +2,7 @@ import Content from "./Content.jsx";
 
 module.exports = {
   id: "indonesiaTravelThoughts",
+  tags: ["indonesia", "travel thoughts", "post", "q&A", "question", "question and answer", "translation", "google translate", "communication", "sunburn", "crying", "fairy peacock", "honey bear", "animals"],
   title: "Indonesia Travel Thoughts",
   tagLine: "Q&A",
   datePublished: "8 August, 2017",
