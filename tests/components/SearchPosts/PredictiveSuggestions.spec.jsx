ddfa47012f705a96afd7d121a58d474c3b8a8f44
syneva-runyan <syneva@gmail.com>
2017-08-24 23:28:57 -0400

    finished predictive search styling and functionailyt and tests

--- tests/components/SearchPosts/PredictiveSuggestions.spec.jsx
+++ tests/components/SearchPosts/PredictiveSuggestions.spec.jsx
@@ -4,8 +4,18 @@ import { shallow } from "enzyme";
 
 
 describe('PredictiveSuggestions', () => {
-    let component = shallow(<PredictiveSuggestions />)
+    let component = shallow(<PredictiveSuggestions />);
+    const exSuggestion = {
+        id: "someId",
+        title: "someTitle",
+    }
     it("should exist", () => {
         expect(component).toBeTruthy();
-    })
+    });
+
+    it("should display an article link for each provided suggestion", () => {
+        component = shallow(<PredictiveSuggestions suggestions={[exSuggestion, exSuggestion, exSuggestion]}/>);
+        const articleLinks = component.find(".predictiveSuggestions__link");
+        expect(articleLinks.length).toEqual(3);
+    });
 });
--- tests/components/SearchPosts/SearchPosts.spec.jsx
+++ tests/components/SearchPosts/SearchPosts.spec.jsx
@@ -7,6 +7,10 @@ jest.mock("../../../source/js/utils/search-posts");
 
 describe('Search Posts', () => {
 	let component;
+	const exPost = {
+		id: "id",
+		title: "someTitle",
+	}
 
 	beforeEach(() => {
 		component = shallow(<SearchPosts />);
@@ -14,6 +18,15 @@ describe('Search Posts', () => {
 	it('should exist', () => {
 		expect(component).toBeTruthy();
 	});
+	it('should clear current search term and predictive suggestions with closeSearch', () => {
+		component.instance().state.searchTerm = "someTerm";
+		component.instance().state.predictiveSuggestions = [exPost];
+
+		component.instance().closeSearch();
+
+		expect(component.instance().state.searchTerm).toEqual("");
+		expect(component.instance().state.predictiveSuggestions).toEqual(null);
+	})
 
 	it('should call catalog search if text field updates with more than specified preditiveSearchStart chars', () => {
 		const catalogSearcher = spyOn(component.instance(), "searchPosts");
