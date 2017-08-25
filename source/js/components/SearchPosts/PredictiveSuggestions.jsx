cd79737d58888e2b224f26aa6ccb3f3883bf4e5a
syneva-runyan <syneva@gmail.com>
2017-08-24 22:08:11 -0400

    styled search area for mobile

--- source/js/components/SearchPosts/PredictiveSuggestions.jsx
+++ source/js/components/SearchPosts/PredictiveSuggestions.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function PredictiveSuggestions() {
+const propTypes = {
+    suggestions: PropTypes.array
+}
+const defaultProps = {
+    suggestions: null
+}
+
+export const renderSearchLink = function(suggestions) {
+    return (
+        <ul className="predictiveSuggestions__list">
+            <li>Links to related posts go here</li>
+        </ul>
+    )
+}
+
+export const renderNoSuggestions = function() {
+    return (<p className="predictiveSuggestions__none">There are no posts that match your search query</p>)
+}
+export default function PredictiveSuggestions(props) {
     return (
         <div className="predictiveSuggestions">
-            Predictive Suggestions Will Go Here
+            {props.suggestions ? 
+                renderSearchLink(props.suggestions) :
+                renderNoSuggestions()
+            }
         </div>
     )
 }
+
+PredictiveSuggestions.propTypes = propTypes;
+PredictiveSuggestions.defaultProps = defaultProps;
