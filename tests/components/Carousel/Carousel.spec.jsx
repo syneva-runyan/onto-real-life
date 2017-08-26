2088c947e8a25ebc8ec636113782b7c33f447e3d
syneva-runyan <syneva@gmail.com>
2017-08-25 21:33:36 -0400

    added ignore for const use for linter in node script

--- tests/components/Carousel/Carousel.spec.jsx
+++ tests/components/Carousel/Carousel.spec.jsx
@@ -5,25 +5,28 @@ import { shallow, render } from "enzyme";
 jest.mock("../../../source/js/utils/post-helper");
 
 describe("Post Content", () => {
-    let component;
+  let component;
   const exSlide = "Ex Slide content";
-  const mockSlides = [exSlide, exSlide, exSlide] ;
-  const exAniDuration = 2000
+  const mockSlides = [exSlide, exSlide, exSlide];
+  const exAniDuration = 2000;
 
   beforeEach(() => {
-    component = shallow(<Carousel aniDuration={exAniDuration} slides={mockSlides} />);
+    component = shallow(
+      <Carousel aniDuration={exAniDuration} slides={mockSlides} />,
+    );
   });
   it("should exist", () => {
-    expect(component).toBeTruthy();
+    //expect(component).toBeTruthy();
+    expect(true).toEqual(false);
   });
 
-  it("should call setInterval when component is mounted", ()=> {
+  it("should call setInterval when component is mounted", () => {
     const spy = spyOn(window, "setInterval");
     component.instance().componentDidMount();
     expect(spy).toHaveBeenCalled();
   });
 
-  it("should call clearInterval when component is mounted", ()=> {
+  it("should call clearInterval when component is mounted", () => {
     const spy = spyOn(window, "clearInterval");
     component.instance().componentWillUnmount();
     expect(spy).toHaveBeenCalled();
@@ -39,27 +42,27 @@ describe("Post Content", () => {
   });
 
   it("should generate a slide for every 'slide'", () => {
-   const slides = component.find(".carousel__slide");
+    const slides = component.find(".carousel__slide");
 
-   expect(slides.length).toEqual(3);
+    expect(slides.length).toEqual(3);
   });
 
   it("should make prev and next slides identifiable", () => {
     component.instance().setState({
-      active: 1
+      active: 1,
     });
     const prev = component.find(".carousel__slide--prev");
     const next = component.find(".carouesl__slide--next");
 
-    expect(prev.node.key).toEqual('slide-0');
-    expect(next.node.key).toEqual('slide-2');
+    expect(prev.node.key).toEqual("slide-0");
+    expect(next.node.key).toEqual("slide-2");
   });
-  it("should wrap next and prev slide identifiers as appropriate to active index", ()=> {
+  it("should wrap next and prev slide identifiers as appropriate to active index", () => {
     component.instance().setState({
-      active: 2
+      active: 2,
     });
 
     const next = component.find(".carouesl__slide--next");
-    expect(next.node.key).toEqual('slide-0');
-  })
+    expect(next.node.key).toEqual("slide-0");
+  });
 });
