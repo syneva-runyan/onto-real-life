0c6533b8ba7f03d1ad39a959cdbd1bba06b1dd77
syneva-runyan <syneva@gmail.com>
2017-09-15 01:27:29 -0400

    added a basic social share comp skelaton

---
+++ source/js/components/Global/SocialShares.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function SocialShares() {
+  return (
+    <ul className="socialShares">
+      <li className="socialShares__icon socialShares__icon--facebook">
+        <span>Share on Facebook</span>
+      </li>
+      <li className="socialShares__icon socialShares__icon--twitter">
+        <span>Share on Twitter</span>
+      </li>
+      <li className="socialShares__icon socialShares__icon--instagram">
+        <span>Follow on Instagram</span>
+      </li>
+    </ul>
+  );
+}
