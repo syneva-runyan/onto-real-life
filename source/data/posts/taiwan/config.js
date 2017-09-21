2d0a9bd6527414d2793a684ad7f6391a746bde9f
syneva-runyan <syneva@gmail.com>
2017-09-20 23:55:27 -0400

    finished taiwan post outline

--- source/data/posts/taiwan/config.js
+++ source/data/posts/taiwan/config.js
@@ -2,7 +2,7 @@ import Content from "./Content.jsx";
 
 module.exports = {
   id: "taiwan",
-  tags: ["taiwan", "taipei", "juifen", "spirited away", "chinese", "selfie coffee", "coffee", "aborigines", "formosa", "biking", "bike", "cycle", "hike", "hiking", "gorge", "coast", "juifen", "lanterns", "ghost", "festival"],
+  tags: ["taipei", "juifen", "spirited away", "chinese", "selfie coffee", "coffee", "aborigines", "formosa", "biking", "bike", "cycle", "hike", "hiking", "gorge", "coast", "juifen", "lanterns", "ghost", "festival"],
   title: "Taiwan",
   tagLine: "The Beautiful Island",
   datePublished: "18 September, 2017",
