bb5f5ad52cd177a1b5118f18fe6e36bb86f04ac1
syneva-runyan <syneva@gmail.com>
2017-09-20 23:55:27 -0400

    finished taiwan post outline

--- source/data/posts/taiwan/Content.jsx
+++ source/data/posts/taiwan/Content.jsx
@@ -3,7 +3,63 @@ import React from "react";
 export default function Content(props) {
   return (
     <div>
-        <p>Content</p>
+        <blockquote>Beutiful scenery, brilliant history, coastal biking, bubble tea, and nightmarkets (each with better food than the last).</blockquote>
+        <p>Each of these aspects of Taiwan make the island a brilliant holiday destination.</p>
+        <p>The land mass of Taiwan covers just barely over 13,000 square miles.  In that area there are no big-name attractions that capture social media's attention.</p>
+        <p>The prices in Taiwan, while inexpensive compared to the US or Australia, are slightly higher than its South East Asian neighbors.</p>
+        <p>Both of these points keep Taiwan off most travelers' radars.  This lack of overblown tourism, in turn, contributes to the island's charm.</p>
+        <p>Culturally facscinating and aestetically stunning, Taiwan is a hidden travel gem.</p>
+        <h3>Culture</h3>
+        <p>In addition to aboriginal tribes on the island, Taiwan was once a Dutch colony, became a haven for fleeing Ming Dynasty loyalists, taken over by the Qing dynasty, was ruled by the Japanese prior and throughout WWII, and then officially became a part of China.</p>
+        <p>Today, Taiwan is a self governing province of China, although if you speak to any younger Taiwanese, you'll notice an open minded Western attitude that distinguishes the Taiwanese from the Chinese.</p>
+        <blockquote>The relationship between Taiwan and mainland China is somewhat precarious, and politically <a href="">fascinating to learn about.</a></blockquote>
+        <h4>Aborigonies</h4>
+        <p>Despite a history of aboriginal oppression, the indigenous cultures and traditions are increasingly celebrated on the island.</p>
+        <p>For instance, there is a month-long Ghost-Festival that happened to coincide with my visit.</p>
+        <blockquote>During the Ghost Festival, _______________.</blockquote>
+        <p>Owls, a symbol of __________, can be found everywhere, especially in the souvenir shops at Sun Moon Lake.</p>
+        <p>Art collectives and ceramics workshops _____</p>
+        <h4>Chinese Influence</h4>
+        <p>While the precence of aboriginal culture is ever increasing, it is heavily outweighed by Chinese culture and traditions on the island.</p>
+        <p>The soft glow of red lanterns, elaborate temples, and delicious dumplings are omnipresent.</p>
+        <p>The streets of Juifen in the North exemplified the windey streets and glowing lanterns I have always pictured belonging in Asian villages.</p>
+        <blockquote>The Japanese director ____ Mirazaki apparently agrees, as the town was used as an inspiration for the Ghost Town in his movie <em>Spirited Away.</em></blockquote>
+        <p>Longshan temple in Taipei is just one of many elaborate and peaceful temples around the island.</p>
+        <p>One of my favorite religious items I saw while traveling was a giant statue of a Chinese diety outside of Juifen.</p>
+        <p>Want a cheap, delicious dinner? You can find dumpling restaurants or stands on almost every corner of every city.</p>
+        <p>The culinary tasts of Taiwan however expands so much further than the chinese-influenced dishes.  Red Yeast Meatballs, Sweet Taro Soup, and oyster omlets are a small handful of food choices you can find in night markets.</p>
+        <p>Per the culture, most menus will be in Chinese, only some with pictures.  It's beyond helpful to get food recomendations from a hotel or hostel before going out to eat, to bring a Chinese speaking friend with you, or to try using Google Translate's somewhat funky Traditional Chinese translations.</p>
+        <h4>City Life</h4>
+        <p>It's so easy to fall in love with Taipei's skyline.</p>
+        <blockquote>The sunset I watched from Elephant Mountain belongs at the end of a Nicholas Sparks movie.</blockquote>
+        <p>Diving in between the buildings, nightmarkets are full of energy, delicious, cheap food, and an upbeat rhythm of life.</p>
+        <p>The engery carries over to interesting cafes and people.</p>
+        <p>There are cafes in Taipei where you can get your face printed on a cup of coffee.</p>
+        <p>I saw a cat with his own QR code being carried around Sheman in Taipei. <em>I abruptly turned around and followed the cat to take this creeper picture.</em></p>
+        <blockquote>If you see a stroller on the street, its just as likely to have a little white dog sitting inside it as a baby.</blockquote>
+        <p>The subway and bus systems around cities were extremely organized and easy to use.</p>
+        <p>The culture of Taiwanese cities is organized, uniquely beautiful, and exciting.</p>
+        <h3>Aesthetic Beauty</h3>
+        <p>From brilliantly beaches to towering mountains and gorges, Taiwan has it all.</p>
+        <p>The Zhulia Old Trail in Taroko Gorge is one of my favorite hikes I've done on this trip.</p>
+        <p>The scenic east coast in perfect for a several day biking expedition between Hualien and Taitung <em>*Disclaimer - We drive.  SO and thankyou again Aaron!</em></p>
+        <p>Uncrowded sandy Beaches and warm water make Taiwan a perfect surfer haven.<em>Disclaimer - I also can't surf.</em></p>
+        <p>For more extreme adventurers, the _____ mountain in the middle of Taiwan is _____. <em>Discaimer - I also didn't climb this.  What did I even do with my time in Taiwan again?</em></p>
+        <p></p>
+        <h4>The Beautiful Island</h4>
+        <p>Taiwan is a perfect island for both cultural and outdoor adventuring. Taipei is the first city I've been to on this trip that I actually felt like I could live in <em>Chinese defffinnnattteellyy needs some work though. My pronunciation is so bad that I had to apologize everytime I tried to speak.</em>.</p>
+        <p>I feel like it's only a matter of time before more people discover this Beautiful Island.</p>
+        <p><em>Cue beautiful sunset from Elephant Mountain</em></p>
+        <hr/>
+        <h4>Other Notes</h4>
+        <ul>
+          <li>Thank you Laurie for being an awesome travel buddy, follow her travel adventures on instagram <a>here.</a></li>
+          <li>Thank you Michael for being an awesome host in Taipei and an awesome person in general!</li>
+          <li><strong>Beautiful Island - </strong>I reference Taiwan as the Beautiful island several times in this post and in the tagline. <em>Beautiful Island</em> comes the Portugese word <em>Formosa,</em> which is a name ____ gave to Taiwan in ______.</li>
+          <li><strong>Shongshan Culture Park - </strong> Really awesome creative market in Taipei not to be missed by anyone who appreciates crafting and culture.</li>
+          <li><strong>Bubble tea</strong> was invented in Taiwan ________.</li>
+          <li>Can I just bring up how awesome the <strong>Night Markets</strong> were again?</li>
+        </ul>
     </div>
   );
-}
+};
