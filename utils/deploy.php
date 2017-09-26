8afdf471c546a38692cbc2aa3b79a789352b53b7
syneva-runyan <syneva@gmail.com>
2017-09-26 06:15:42 -0400

    adding base deployment script for testing

--- utils/deploy.php
+++ utils/deploy.php
@@ -1,2 +1,50 @@
 <?php
-$target_dir = "ontoreallife/";
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+
+	// send email to me when updates are made.
+	// code cred synevarunyan
+	$email_to = "syneva@gmail.com";	
+	$email_subject = "OntoReallife Deployment";
+	$email_message = $output;
+	$headers = 'From: '.$email_from."\r\n".
+	
+	'Reply-To: '.$email_from."\r\n" .
+	
+	'X-Mailer: PHP/' . phpversion();
+	
+	if(mail($email_to, $email_subject, $email_message, $headers))
+	{
+		//handle success
+	}
+	else
+	{
+		//handle failure
+	}
+?>
