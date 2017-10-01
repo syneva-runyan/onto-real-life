67dedcedd43c20a977e2d77379c1ab6cbd5be244
syneva-runyan <syneva@gmail.com>
2017-10-01 05:16:59 -0400

    finised cambodia post content

---
+++ source/data/posts/cambodia/config.js
@@ -0,0 +1,10 @@
+import Content from "./Content.jsx";
+
+module.exports = {
+  id: "cambodia",
+  tags: ["asia", "south east asia", "temples", "siem reap", "phnom pehn"],
+  title: "Cambodia",
+  tagLine: "Can I be Lara Croft?",
+  datePublished: "30 September 2017",
+  component: Content,
+};
