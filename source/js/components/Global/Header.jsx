4aa167aed3a74797e847f54f4bc933fbb13818f8
Syneva <syneva@gmail.com>
2019-02-28 11:01:35 -0500

    fixed mobile menu

--- source/js/components/Global/Header.jsx
+++ source/js/components/Global/Header.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, createHook } from "react";
 import PropTypes from "prop-types";
 import Title from "./Title";
 import Menu from "./Menu";
+import { debounce } from "lodash";
 
 const propTypes = {
   location: PropTypes.object,
@@ -14,13 +15,32 @@ const defaultProps = {
 };
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.mobileScrollInit()
+    this.state = {
+      mobileShift: false
+    }
+  }
+
+  mobileScrollInit() {
+    window.addEventListener('scroll', debounce(this.checkForScroll))
+  }
+
+  checkForScroll = () => {
+    if(document.documentElement.scrollTop > 0 && !this.state.mobileShift) {
+      this.setState({ mobileShift: true});
+    } else if (document.documentElement.scrollTop == 0 && this.state.mobileShift) {
+      this.setState({ mobileShift: false});
+    }
+  }
   render() {
     return (
       <div className="header">
         <div className="header__box">
           <div className="header__section">
             <span className="header__logo" />
-            <Title />
+            <Title mobileShift={this.state.mobileShift} />
           </div>
           <div className="header__section header__section--lg">
             <Menu />
